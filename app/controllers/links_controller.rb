4e196bf68e31134ab3a675aeb31c87e38e04f71d
Artem <skrartrom@icloud.com>
2020-09-11 19:33:18 +0300

    add style

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,7 +5,7 @@ class LinksController < ApplicationController
   # GET /links
   # GET /links.json
   def index
-    @links = Link.all.paginate(page: params[:page], per_page: 3).order(:created_at)
+    @links = Link.all.paginate(page: params[:page], per_page: 6).order(:created_at)
   end
 
   # GET /links/1
