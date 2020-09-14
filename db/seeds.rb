843715c8ef03350f6fedb756ba57daa2db39c7de
Artem <skrartrom@icloud.com>
2020-09-14 09:35:45 +0300

    update for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
-Link.create(full_name: 'https://github.com/')
-Link.create(full_name: 'https://www.google.com.ua/?hl=ru')
-Link.create(full_name: 'https://www.youtube.com/?hl=uk&gl=UA')
+5.times do
+  Link.create(full_name: 'https://github.com/')
+  Link.create(full_name: 'https://www.google.com.ua/?hl=ru')
+  Link.create(full_name: 'https://www.youtube.com/?hl=uk&gl=UA')
+end
